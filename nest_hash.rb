668df3f9193738e3e39b59b94d52b474c4245e89
caitlinmch <caitlinmch@me.com>
2018-06-06 21:29:44 +0000

    automatically backed up by learn

--- nest_hash.rb
+++ nest_hash.rb
@@ -3,6 +3,6 @@ closet={
   "bottoms"=>["jeans","skirt","shorts"],
   "shoes"=>["nike","vans","sandals"],
 }
-puts closet["tops"] # this prints out just my tops 
+#puts closet["tops"] # this prints out just my tops 
  puts closet["tops"]<< "blouse"
 
