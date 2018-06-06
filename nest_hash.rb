2b3357c0e54605bdb3fee5974d82cc16165c8948
caitlinmch <caitlinmch@me.com>
2018-06-06 21:39:05 +0000

    automatically backed up by learn

--- nest_hash.rb
+++ nest_hash.rb
@@ -5,4 +5,6 @@ closet={
 }
 #puts closet["tops"] # this prints out just my tops 
 # puts closet["tops"]<< "blouse"
+puts closet["shoes"]["misc"]=["cheetah slippers", "rainboots"]
+puts closet
 # puts closet["tops"].insert(4, "tube-top")
