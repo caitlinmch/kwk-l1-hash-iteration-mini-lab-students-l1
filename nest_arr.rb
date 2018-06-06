91ab3c1f4270792e21ef135243271a432afed22e
caitlinmch <caitlinmch@me.com>
2018-06-06 20:50:17 +0000

    automatically backed up by learn

--- nest_arr.rb
+++ nest_arr.rb
@@ -3,8 +3,15 @@ closet=[
   ["jeans","skirt","capris"],
   ["sneakers","heels","flats"]
   ]
+  # puts closet #shows everything in the main Array
+  #puts closet[0] #shows only things in the first array 
   
   
-  
-  
-  tops=
+  tops= ["rain jacket","cardigan","blazer"],
+ bottomes= ["jeans","skirt","capris"],
+ shoes= ["sneakers","heels","flats"]
+  closet=[tops, bottoms,shoes]
+  puts closet
+  shoes<< "nike"
+  shoes.push("vans")
+  puts closet
