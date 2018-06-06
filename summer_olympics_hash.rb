32ab595d998528de783b1250f8fa105c1eb46ea2
caitlinmch <caitlinmch@me.com>
2018-06-06 20:23:53 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -14,7 +14,7 @@ def add_a_key_value_pair
   }
 end
 
- create_olympics_hash
+# create_olympics_hash
    
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 
