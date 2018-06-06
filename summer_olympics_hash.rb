42d3219500f0749a28cf5a8ec62e6b8e6fcef62c
caitlinmch <caitlinmch@me.com>
2018-06-06 20:07:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -8,7 +8,9 @@ def add_a_key_value_pair
   olympic_hash={
     "Sydney"=>"2000"
     "Athens"=>"2004"
-    "Bejing"=>""
+    "Bejing"=>"2008"
+    "london"=>"2012"
+    
   }
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
